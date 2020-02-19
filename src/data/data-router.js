4055c88b6a263a8c7f80e2b4f42a734428d6540f
zoeferencova <zoeferencova@gmail.com>
2020-02-19 11:40:58 -0500

    pre-transaction refactor

--- src/data/data-router.js
+++ src/data/data-router.js
@@ -6,24 +6,7 @@ const { requireAuth } = require('../middleware/jwt-auth')
 
 const dataRouter = express.Router();
 
-const timeSpans = []
-const units = {
-    days: 7,
-    weeks: 4,
-    months: 6
-}
 
-for (let [key, value] of Object.entries(units)) {
-    const unit = key;
-    const number = value;
-
-    for (let i=0; i < number; i++) {
-        timeSpans.push({
-            start: moment().startOf(`${unit}`).subtract(i+1, `${unit}`).toISOString(),
-            end: moment().startOf(`${unit}`).subtract(i, `${unit}`).toISOString()
-        })
-    }
-}
 
 dataRouter
     .route('/pm-data')
@@ -45,12 +28,23 @@ dataRouter
         const bearerToken = authToken.slice(7, authToken.length)
         const payload = AuthService.verifyJwt(bearerToken);
         const userId = payload.user_id;
-        const arr = []
-        timeSpans.forEach((span) => {
-            arr.push(DataService.getTimespanData(req.app.get('db'), userId, span))
-        })
-        console.log(arr)
-        
+        DataService.getTimespanData(req.app.get('db'), userId)
+            .then(data => {
+                return res.json(data)
+            })
+    })
+
+dataRouter
+    .route('/time-completed-data')
+    .get((req, res, next) => {
+        const authToken = req.get('Authorization');
+        const bearerToken = authToken.slice(7, authToken.length)
+        const payload = AuthService.verifyJwt(bearerToken);
+        const userId = payload.user_id;
+        DataService.getTimeCompletedData(req.app.get('db'), userId)
+            .then(data => {
+                return res.json(data)
+            })
     })
 
 module.exports = dataRouter;
--- src/data/data-service.js
+++ src/data/data-service.js
@@ -1,5 +1,45 @@
 const moment = require('moment')
 
+const timeSpans = [
+    {
+        start: moment().startOf('day').toISOString(),
+        end: moment().toISOString(),
+        unit: 'days',
+        number: 0
+    },
+    {
+        start: moment().startOf('week').toISOString(),
+        end: moment().toISOString(),
+        unit: 'weeks',
+        number: 0
+    },
+    {
+        start: moment().startOf('month').toISOString(),
+        end: moment().toISOString(),
+        unit: 'months',
+        number: 0
+    },
+]
+const units = {
+    days: 7,
+    weeks: 4,
+    months: 6
+}
+
+for (let [key, value] of Object.entries(units)) {
+    const unit = key;
+    const number = value;
+
+    for (let i=1; i < number; i++) {
+        timeSpans.push({
+            start: moment().startOf(`${unit}`).subtract(i+1, `${unit}`).toISOString(),
+            end: moment().startOf(`${unit}`).subtract(i, `${unit}`).toISOString(),
+            unit,
+            number: i,
+        })
+    }
+}
+
 const DataService = {
     getItemCountByPm(db, userId) {
         return db
@@ -12,42 +52,42 @@ const DataService = {
             .groupBy('coordinator_pms.pm_name')
             .groupBy('coordinator_list_items.pm_id')
     },
-    getTimespanData(db, userId, span) {
-            return db
-                .from('coordinator_list_items')
-                .where('coordinator_list_items.date_created', '>', span.start)
-                .where('coordinator_list_items.date_created', '<', span.end)
-                .where('coordinator_list_items.user_id', '=', userId)
-                .count(`coordinator_list_items AS item_count`)
-                .then(data => data[0])
-        
-
-        // return db.transaction(function(trx) {
-        //     db
-        //         .from('coordinator_list_items')
-        //         .transacting(trx)
-        //         .then(function() {
-        //             timeSpans.forEach((span, i) => {
-        //                  db
-        //                     .from('coordinator_list_items')
-        //                     .where('coordinator_list_items.date_created', '>', span.start)
-        //                     .where('coordinator_list_items.date_created', '<', span.end)
-        //                     .where('coordinator_list_items.user_id', '=', userId)
-        //                     .count(`coordinator_list_items AS day${i}`)
-        //         })
-        //         .then(trx.commit)
-        //         .catch(trx.rollback)
-            
-        //     })
-        //     .then(function(response) {
-        //         console.log(response)
-        //     })
-        //     .catch(function(error) {
-        //         console.log(error)
-        //     })
-        // })
-
-            
+    getTimespanData(db, userId) {
+        const data = []
+            return db.transaction(trx => {
+                timeSpans.forEach((span, i) => {
+                    const query = db('coordinator_list_items')
+                    .where('coordinator_list_items.date_created', '>', span.start)
+                    .where('coordinator_list_items.date_created', '<', span.end)
+                    .where('coordinator_list_items.user_id', '=', userId)
+                    .count(`coordinator_list_items AS ${span.unit}${span.number}`)
+                    .transacting(trx)
+                    data.push(query)
+                })
+                
+                Promise.all(data)
+                    .then(trx.commit)
+                    .catch(trx.rollback)
+            })       
+    },
+    getTimeCompletedData(db, userId) {
+        const data = []
+            return db.transaction(trx => {
+                timeSpans.forEach((span, i) => {
+                    const query = db('coordinator_list_items')
+                    .select(db.raw(`EXTRACT(MINUTE FROM (coordinator_list_items.date_completed - coordinator_list_items.date_created)) AS ${span.unit}_${span.number}_difference`))
+                    .where('coordinator_list_items.date_created', '>', span.start)
+                    .where('coordinator_list_items.date_created', '<', span.end)
+                    .where('coordinator_list_items.status', '=', 'completed')
+                    .where('coordinator_list_items.user_id', '=', userId)
+                    .transacting(trx)
+                    data.push(query)
+                })
+                
+                Promise.all(data)
+                    .then(trx.commit)
+                    .catch(trx.rollback)
+            })  
     }
 }
 
