ac1082c5aed4b0b165edcb451ca899cee872b7d1
zoeferencova <zoeferencova@gmail.com>
2021-03-16 18:49:55 -0400

    updated postgrator config

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,5 +6,16 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-  "ssl": !!process.env.SSL,
+  "ssl": process.env.DATABASE_URL ? true : false
 }
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   migrationDirectory: "migrations",
+//   driver: "pg",
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: process.env.DATABASE_URL ? true : false
+// });
+
+// client.connect();
