cf1e0acc07edcdb512b5c9d41e3cfdd47dea02f3
zoeferencova <zoeferencova@gmail.com>
2021-03-17 10:04:12 -0400

    .

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const { PORT, DATABASE_URL } = require('./config');
 
 const db = knex({
     client: 'pg',
-    connection: {DATABASE_URL, ssl: true},
+    connection: DATABASE_URL + "?ssl=true?debug=true",
     ssl: { rejectUnauthorized: false }
 })
 
